a7cd44818fe0257b650350fda6f658a859003ea5
Softcents Systems <softcents@gmail.com>
2026-09-22 21:02:08 +0600

    Connect SMS billing to tariff service

--- apps/api/src/sms/sms.module.ts
+++ apps/api/src/sms/sms.module.ts
@@ -3,9 +3,10 @@ import { SmsController } from './sms.controller';
 import { SmsService } from './sms.service';
 import { AuthModule } from '../auth/auth.module';
 import { QueueModule } from '../queue/queue.module';
+import { TariffsModule } from '../tariffs/tariffs.module';
 
 @Module({
-  imports: [AuthModule, QueueModule],
+  imports: [AuthModule, QueueModule, TariffsModule],
   controllers: [SmsController],
   providers: [SmsService],
 })
