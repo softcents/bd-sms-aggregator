4ba1d89160a5835939af5ead45e8734b56ec8dbe
Softcents Systems <softcents@gmail.com>
2026-09-22 13:50:48 +0600

    feat: add billing and routing foundation

---
+++ apps/api/src/billing/billing.module.ts
@@ -0,0 +1,1 @@
+import { Module } from '@nestjs/common'; import { BillingService } from './billing.service'; @Module({providers:[BillingService],exports:[BillingService]}) export class BillingModule {}
