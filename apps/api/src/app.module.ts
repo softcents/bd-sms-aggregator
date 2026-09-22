c0b8c17d240d5e36d7c35f7ba997925532937bfa
Softcents Systems <softcents@gmail.com>
2026-09-22 13:47:57 +0600

    feat: route SMS API through RabbitMQ

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,1 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { SmsModule } from './sms/sms.module';
-import { InfozillionModule } from './infozillion/infozillion.module';
-
-@Module({imports:[ConfigModule.forRoot({isGlobal:true}),InfozillionModule,SmsModule]})
-export class AppModule {}
+import { Module } from '@nestjs/common'; import { ConfigModule } from '@nestjs/config'; import { SmsModule } from './sms/sms.module'; import { InfozillionModule } from './infozillion/infozillion.module'; import { QueueModule } from './queue/queue.module'; import { AuthModule } from './auth/auth.module'; @Module({imports:[ConfigModule.forRoot({isGlobal:true}),AuthModule,QueueModule,InfozillionModule,SmsModule]}) export class AppModule {}
