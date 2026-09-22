15900c21397475fa1af9455c84b82f6208bb723b
Softcents Systems <softcents@gmail.com>
2026-09-22 20:04:40 +0600

    Register API authentication module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [HealthController],
+  imports: [DatabaseModule, AuthModule],
+  controllers: [HealthController, AuthController],
 })
 export class AppModule {}
