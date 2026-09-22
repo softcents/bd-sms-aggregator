eb0e163321aa084859fe41db63815bdf29091a25
Softcents Systems <softcents@gmail.com>
2026-09-22 19:59:07 +0600

    Register database module in API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,1 +1,9 @@
-import {Module} from '@nestjs/common';import {HealthController} from './health.controller';@Module({controllers:[HealthController]})export class AppModule{}
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database.module';
+import { HealthController } from './health.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [HealthController],
+})
+export class AppModule {}
