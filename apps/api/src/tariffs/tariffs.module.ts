b0d99a2936619a34d29699a3d604aa01c2dcbf2d
Softcents Systems <softcents@gmail.com>
2026-09-22 20:38:53 +0600

    Add tariff and operator service

---
+++ apps/api/src/tariffs/tariffs.module.ts
@@ -0,0 +1,4 @@
+import { Module } from '@nestjs/common';
+import { TariffsController } from './tariffs.controller';
+import { TariffsService } from './tariffs.service';
+@Module({controllers:[TariffsController],providers:[TariffsService]}) export class TariffsModule {}
