28691bdd3bdab79fd655db34580a3aecfd3f4953
Softcents Systems <softcents@gmail.com>
2026-09-22 20:12:20 +0600

    Register SMS and queue modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,11 @@ import { DatabaseModule } from './database.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { HealthController } from './health.controller';
+import { QueueModule } from './queue/queue.module';
+import { SmsModule } from './sms/sms.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, QueueModule, SmsModule],
   controllers: [HealthController, AuthController],
 })
 export class AppModule {}
