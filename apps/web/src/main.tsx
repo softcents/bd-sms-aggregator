df81fca6b2a5b14d75ff3db983b9e6c78a660a51
Softcents Systems <softcents@gmail.com>
2026-09-22 19:35:36 +0600

    feat: initialize clean platform foundation

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,1 @@
+import React from'react';import{createRoot}from'react-dom/client';function App(){return <main><h1>BD SMS Aggregator</h1><p>InfoZillion SMS Platform</p></main>}createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
