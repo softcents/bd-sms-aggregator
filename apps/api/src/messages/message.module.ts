2873094f895ad7d3ccc4cec2f65621eb0941da99
Softcents Systems <softcents@gmail.com>
2026-09-22 13:54:20 +0600

    feat: add message persistence and DLR foundation

---
+++ apps/api/src/messages/message.module.ts
@@ -0,0 +1,1 @@
+import { Module } from '@nestjs/common'; import { MessageRepository } from './message.repository'; @Module({providers:[MessageRepository],exports:[MessageRepository]}) export class MessageModule {}
